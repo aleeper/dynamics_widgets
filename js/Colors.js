40dcf73d55c06adc4fbd03d2ef8d158337de25b6
Adam Leeper <aleeper@hidof.com>
2014-10-13 09:17:18 -0700

    add the rest of the stuff

---
+++ js/Colors.js
@@ -0,0 +1,18 @@
+
+
+var MAT_BLACK   = new THREE.MeshPhongMaterial( { ambient :0x000000, color: 0x222222, specular: 0x111111 } );
+var MAT_WHITE   = new THREE.MeshPhongMaterial( { ambient :0xFFFFFF, color: 0xFFFFFF, specular: 0xFFFFFF } );
+var MAT_SILVER  = new THREE.MeshPhongMaterial( { ambient :0xC0C0C0, color: 0xC0C0C0, specular: 0xC0C0C0 } );
+var MAT_GRAY    = new THREE.MeshPhongMaterial( { ambient :0x808080, color: 0x808080, specular: 0x808080 } );
+var MAT_RED     = new THREE.MeshPhongMaterial( { ambient :0xFF0000, color: 0xFF0000, specular: 0xFF0000 } );
+var MAT_GREEN   = new THREE.MeshPhongMaterial( { ambient :0x00FF00, color: 0x00FF00, specular: 0x00FF00 } );
+var MAT_BLUE    = new THREE.MeshPhongMaterial( { ambient :0x0000FF, color: 0x0000FF, specular: 0x0000FF } );
+var MAT_CHOCOLATE    = new THREE.MeshPhongMaterial( { ambient :0xCD661D, color: 0xCD661D, specular: 0xCD661D } );
+
+var SHINY_BLACK   = new THREE.MeshPhongMaterial( { ambient :0x000000, color: 0x222222, specular: 0xFFFFFF } );
+var SHINY_WHITE   = new THREE.MeshPhongMaterial( { ambient :0xFFFFFF, color: 0xFFFFFF, specular: 0xFFFFFF } );
+var SHINY_SILVER  = new THREE.MeshPhongMaterial( { ambient :0xC0C0C0, color: 0xC0C0C0, specular: 0xFFFFFF } );
+var SHINY_GRAY    = new THREE.MeshPhongMaterial( { ambient :0x808080, color: 0x808080, specular: 0xFFFFFF } );
+var SHINY_RED     = new THREE.MeshPhongMaterial( { ambient :0xFF0000, color: 0xFF0000, specular: 0xFFFFFF } );
+var SHINY_GREEN   = new THREE.MeshPhongMaterial( { ambient :0x00FF00, color: 0x00FF00, specular: 0xFFFFFF } );
+var SHINY_BLUE    = new THREE.MeshPhongMaterial( { ambient :0x0000FF, color: 0x0000FF, specular: 0xFFFFFF } );
